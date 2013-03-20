7de1bc188885e6b23121782f374e4523a77642c1
Jeffrey Lang <jeffrey@1socialmedia.nl>
2013-03-20 16:26:14 +0100

    Ticket Tab

--- application/views/website.php
+++ application/views/website.php
@@ -2,20 +2,113 @@
 body{
 	overflow: hidden;
 }
-iframe{   top:0; 
-		left:0; 
-		width:127%; 
-		height:120%; 
-		position:absolute;
-		 zoom: 0.20;
-		-moz-transform:scale(0.80);
-		moz-transform-origin: 0 0;
-		-o-transform: scale(0.80);
-		o-transform-origin: 0 0;
-		-webkit-transform: scale(0.80);
-		-webkit-transform-origin: 0 0;
-		overflow-y: auto;
+.wrap{
+	width: 810px;
+	margin: 0 auto;
 }
 
+.wrapframe{
+	width:400px;
+	position: relative;
+	float: left;
+}
+
+.info{
+	width: 380px;
+	position: relative;
+	float: left;
+	margin-left: 20px;
+}
+
+.info_box
+{
+	background-color: #E3E8FC;
+    border-radius: 10px 10px 10px 10px;
+    color: #333333;
+    float: left;
+    margin: 3px 0 0;
+    padding: 6px 2px 10px 10px;
+    width: 370px;
+    margin-top: 10px;
+}
+
+.info img{
+	width: 380px;
+}
+a{
+	color:#7D7D7D;
+	text-decoration: none;
+}
+iframe{   
+		
+		width:400px; 
+		height:100%; 
+	
+		overflow-y: auto;
+}
+h3.online{
+	font-family: "Trebuchet MS",Arial,Helvetica,san-serif;
+    font-weight: normal;
+    letter-spacing: 0.4px;
+    line-height: 1.4em;
+    padding-bottom: 2px;
+    position: relative;
+       color: #7890D2;
+}
+h3{
+	font-family: "Trebuchet MS",Arial,Helvetica,san-serif;
+    font-weight: normal;
+    letter-spacing: 0.4px;
+    line-height: 1.4em;
+    padding-bottom: 2px;
+    position: relative;
+       color: #5E5E5E;
+}
+p{
+	color: #7D7D7D;
+    font-size: 12px;
+}
 </style>
-<iframe src="http://www.blueboat.nl/" scrolling="no" frameborder="0"></iframe>
+
+<div class="wrap">
+	<div class="wrapframe">
+		
+<h3 class="online">Online Tickets</h3>
+<p>
+Natuurlijk is het mogelijk om online alvast kaartjes te kopen voor één van onze reguliere rondvaarten! In onderstaande webshop kunt u kaartjes kopen voor alle reguliere rondvaarten.
+</p>
+		<iframe src="http://www.link2ticket.nl/ordermain.aspx?link=A1hXcUMFflhAVQ&logo=0" scrolling="no" frameborder="0"></iframe>
+	</div>
+	<div class="info">
+		<img src="/themes/1socialmedia/images/370998.ccckaartjes.jpg">
+	
+	<div class="info_box">
+		
+<h3>Belangrijke informatie over E-tickets</h3>
+<p>Controleer altijd de datum, tijd en soort vaart voor u de bestelling definitief maakt. Eenmaal gekochte kaarten kunnen niet retour genomen worden!</p>
+
+<h3>E-tickets voor de City Canal Cruise</h3>
+
+<p>De tickets voor de City Canal Cruise zijn open tijd tickets, u kunt dus zelf kiezen met welke afvaart u mee wilt. </p>
+
+<h3>E-tickets voor de Evening Cruise</h3>
+
+<p>De tickets voor de Evening Cruise staan op datum en afvaarttijd. U bent dus gegarandeerd van plek op die betreffende datum en tijd. Wel dient u uw e-ticket aan de kassa van Blue Boat Company in te wisselen voor een boarding pass.</p>
+
+<h3>E-tickets voor de Dinner Cruise</h3>
+
+<p>De tickets voor de Blue Boat Dinner Cruise staan op datum. U bent dus gegarandeerd van plek op die betreffende datum en tijd. Wel dient u uw e-ticket aan de kassa van Blue Boat Company in te wisselen voor een boarding pass.
+
+Indien u vegetarisch eet of andere dieetwensen heeft, bijvoorbeeld allergieën, kunt u deze doorgeven aan Blue Boat Company tot 24 uur voor afvaart. Dit kan telefonisch via 020-6791370 of via de mail <a href="mailto:reservations@blueboat.nl">reservations@blueboat.nl</a>.</p>
+
+<h3>Geldigheid tickets</h3>
+<p>
+Alle tickets zijn geldig tot 1 jaar na uitgifte.
+
+Het product is gegarandeerd tot 31 december 2013. Daarna kan een product veranderen of uit het assortiment verdwijnen.
+</p>
+		
+	</div>
+	</div>
+	
+</div>
